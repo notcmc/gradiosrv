39af65bc203051ea5278eeaa3d2af6d616e759c2
notcmc <notcmccauley@gmail.com>
2025-07-11 12:56:45 +0100

    Update serve.py

--- serve.py
+++ serve.py
@@ -1,84 +1,45 @@
-from flask import Flask, make_response, request
-
-app = Flask(__name__)
-
 """
 Beta version of GradioSRV
 """
 
-
-html_template = """
-<html>
-    <head>
-        <title>{title}</title>
-    </head>
-    <body>
-        <h1>{heading}</h1>
-        <p>{info}</p>
-    </body>
-</html>
-"""
+from flask import Flask, request, jsonify, send_file
 
 
-def get_response(title, heading, info, status):
-    return make_response(
-        html_template.format(
-            title=title,
-            heading=heading,
-            info=info
-        ),
-        status
-    )
+app = Flask(__name__)
 
 
 @app.route("/")
 def root():
-    return get_response(
-        "OK",
-        "200 OK",
-        "GradioSRV is online",
-        200
-    )
+    return send_file("validate.html")
 
 
-@app.route("/validate", methods=["GET"])
+@app.route("/validate", methods=["POST"])
 def validate():
-    token = request.args.get("token")
-    role = request.args.get("role")
-
-    if token is None or role is None:
-        return get_response(
-            "Bad Request",
-            "400 Bad Request",
-            "Missing token or role parameter(s)",
-            400
-        )
-
-    if role not in ["Admin", "QA", "Beta"]:
-        return get_response(
-            "Bad Request",
-            "400 Bad Request",
-            "Invalid role",
-            400
-        )
-
-    if len(token) != 37 or (not token.startswith("hf_")):
-        return get_response(
-            "Bad Request",
-            "400 Bad Request",
-            "Malformed HuggingFace token",
-            400
-        )
+    data = request.get_json()
+    if not data or "role" not in data or "token" not in data:
+        return jsonify({
+            "success": False,
+            "message": "Missing token or role parameter(s)"
+        }), 400
     
-    # TODO: perform auth check with HuggingFace and return 401 if invalid
-    # TODO: return temporary API key to user for making genuine requests
-
-    return get_response(
-        "OK",
-        "200 OK",
-        "Validation successful",
-        200
-    )
+    role = data["role"]
+    if role not in ["Admin", "QA", "Beta"]:
+        return jsonify({
+            "success": False,
+            "message": "Invalid role"
+        }), 400
+    
+    token = data["token"]
+    if len(token) != 37 or (not token.startswith("hf_")):
+        return jsonify({
+            "success": False,
+            "message": "Malformed HuggingFace token"
+        }), 400
+    
+    return jsonify({
+        "success": True,
+        "message": "Validation successful"
+    }), 200
 
 
 @app.route("/query", methods=["GET"])
