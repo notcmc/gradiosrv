99ea4c7aa43ac3eb84f62aae03ff4fc49c7372ea
notcmc <notcmccauley@gmail.com>
2025-07-11 11:48:23 +0100

    swap to HTML

--- serve.py
+++ serve.py
@@ -1,14 +1,44 @@
+from flask import Flask, make_response, request
+
+app = Flask(__name__)
+
 """
 Beta version of GradioSRV
 """
 
-from flask import Flask, request
 
-app = Flask(__name__)
+html_template = """
+<html>
+    <head>
+        <title>{title}</title>
+    </head>
+    <body>
+        <h1>{heading}</h1>
+        <p>{info}</p>
+    </body>
+</html>
+"""
+
+
+def get_response(title, heading, info, status):
+    return make_response(
+        html_template.format(
+            title=title,
+            heading=heading,
+            info=info
+        ),
+        status
+    )
+
 
 @app.route("/")
 def root():
-    return "GradioSRV is online", 200
+    return get_response(
+        "OK",
+        "200 OK",
+        "GradioSRV is online",
+        200
+    )
 
 
 @app.route("/validate", methods=["GET"])
@@ -17,17 +47,38 @@ def validate():
     role = request.args.get("role")
 
     if token is None or role is None:
-        return "Missing `token` or `role` parameters", 401
+        return get_response(
+            "Bad Request",
+            "400 Bad Request",
+            "Missing token or role parameter(s)",
+            400
+        )
 
     if role not in ["Admin", "QA", "Beta"]:
-        return "Invalid role", 400
+        return get_response(
+            "Bad Request",
+            "400 Bad Request",
+            "Invalid role",
+            400
+        )
 
     if len(token) != 37 or (not token.startswith("hf_")):
-        return "Invalid HuggingFace token", 401
+        return get_response(
+            "Bad Request",
+            "400 Bad Request",
+            "Malformed HuggingFace token",
+            400
+        )
     
-    # TODO: perform auth check with HuggingFace
+    # TODO: perform auth check with HuggingFace and return 401 if invalid
     # TODO: return temporary API key to user for making genuine requests
-    return "Simple validation performed", 200
+
+    return get_response(
+        "OK",
+        "200 OK",
+        "Validation successful",
+        200
+    )
 
 
 @app.route("/query", methods=["GET"])
